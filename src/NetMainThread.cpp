7803472d3174616a46309a61c0c3a5de15e3a629
moozeq <moozeq@moozeq>
2018-01-20 23:59:35 +0100

    Change inteface eth0 to enp0s3 (vm) and broadcast to 192.168.56.255

--- src/NetMainThread.cpp
+++ src/NetMainThread.cpp
@@ -17,7 +17,7 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 
-#define BROAD "127.255.255.255"
+#define BROAD "192.168.56.255"
 #define PORT 8888   //The port on which to send data
 
 pthread_t receiver;
@@ -104,19 +104,20 @@ ssize_t setAndReceiveInfoMsgUDP(int s, unsigned timeout, InfoMessage * msg) {
 }
 
 struct in_addr getMyIP() {
-	 struct ifreq ifr;
-	 int fd;
+	struct ifreq ifr;
+	int fd;
 
-	 fd = socket(AF_INET, SOCK_DGRAM, 0);
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	 /* I want to get an IPv4 IP address */
-	 ifr.ifr_addr.sa_family = AF_INET;
+	/* I want to get an IPv4 IP address */
+	ifr.ifr_addr.sa_family = AF_INET;
 
-	 /* I want IP address attached to "eth0" */
-	 strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
+	/* I want IP address attached to "eth0" */
+	strncpy(ifr.ifr_name, "enp0s3", IFNAMSIZ-1);
 
-	 ioctl(fd, SIOCGIFADDR, &ifr);
-	 return ((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr;
+	ioctl(fd, SIOCGIFADDR, &ifr);
+	close(fd);
+	return ((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr;
 }
 
 void NetMainThread::receiveNetworkMessages(void) {
